867be31e70a204d10c4d3831ac6da40ad942428d
GanpatHada <hadaganpat42@gmail.com>
2025-02-16 09:54:21 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const errorHandler = require('./middlewares/error.middleware');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use(helmet());
+
+app.get("/",(_, res) => {
+    res.sendFile(path.join(__dirname, "../public", "index.html"));
+  })
+app.use(errorHandler)
+module.exports=app
---
+++ src/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const connectDb = require("./config/db.config.js");
+const app = require("./app");
+const port = process.env.PORT || 8000;
+
+connectDb()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`🚀 server is running on port : ${port}`);
+    });
+  })
+  .catch((e) => {
+    console.log("Mongodb connection failed !!!" + e);
+  });
